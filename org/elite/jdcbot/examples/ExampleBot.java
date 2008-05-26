a269af768e91c3ec0f744f7968f2cbc3f2c9d49f
applegrew <applegrew>
2008-05-26 04:10:48 +0000

    Grouping classes into sub-packages.

---
+++ org/elite/jdcbot/examples/ExampleBot.java
@@ -0,0 +1,119 @@
+/*
+ * ExampleBot.java
+ *
+ * Copyright (C) 2005 Kokanovic Branko
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.elite.jdcbot.examples;
+
+import org.elite.jdcbot.BotException;
+import org.elite.jdcbot.jDCBot;
+import org.elite.jdcbot.util.FloodMessageThread;
+import org.elite.jdcbot.util.GoogleCalculation;
+import org.elite.jdcbot.util.StaticCommands;
+import org.elite.jdcbot.util.TimerThread;
+
+
+/**
+ * ExampleBot is simple derived class from jDCBot apstract class overriding some methods.
+ *
+ * ExampleBot is example and start point for developing your bot. It shows how to overrides jDCBot methods 
+ * and implements simple TimerThread which adds onTimer method.
+ * <p>
+ * Upon connecting, it will announce everyone on hub that it is present
+ * On private message, it will tell that it doesn't understand a word.
+ * Has +calc command on main chat that shows how to use WebPageFetcher and GoogleCalculation
+ * And will flood main chat every 10 min. showing how to use TimerThread class
+ *
+ * @since 0.5
+ * @author Kokanovic Branko, Milos Grbic
+ * @version 0.6
+ */
+public class ExampleBot extends jDCBot{
+    
+    //abstract TimerThread class for flood message
+    private TimerThread tt;
+    
+    private StaticCommands static_cmds=new StaticCommands();
+    
+    public ExampleBot() {
+        //constructs our bot with 100GB share size and 3 slots
+        super("ExampleBot","", "description", "LAN(T1)1","","107374182400", 3);
+        try{
+            connect("10.2.166.131", 411); //my local hub, change it to whatever yours is/
+        }
+        catch(BotException e){
+            System.out.println("Error:"+e);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    
+    /**
+     * Prints on main chat that we are here and starts flood thread.
+     */
+    public void onConnect(){
+        tt=new FloodMessageThread(this, 1000*60*10);
+        tt.start();
+        try{
+            SendPublicMessage("Hi, I'm example jDCBot");
+        }catch(Exception e){}
+    }
+    
+    /**
+     * Simple example how to use GoogleCalculation and static commands from database.
+     * Bot will only detect two commands on main chat:
+     * +help (from database, StaticCommands class) and 
+     * +calc (in form of e.g. '+calc 1+2*3', GoogleCalculation class)
+     */
+    public void onPublicMessage(String user, String message){
+    	//examine if command starts with +calc (you could put more commands here
+    	//in 'else if' construction
+    	if (message.toLowerCase().startsWith("+calc ")){
+			String eq=message.substring(message.indexOf(" ") + 1, message.length());
+			try{
+				Thread t=new Thread(new GoogleCalculation(this,eq));
+				t.start();
+			}catch(Exception e){}
+    	}
+    	//if there were no command recognized above, try to get command from database
+		if (message.startsWith("+")){ //we are calling sql queries only if it starts with '+' for optimization
+			//get only command (not including space if it exist nor anything after it)
+			String only_cmd=((message.indexOf(' '))!=-1)?(message.substring(0,message.indexOf(' '))):message;
+			String output=static_cmds.parseCommand(only_cmd);
+			if (output.length()!=0){ //if command exist in database
+				try{
+					SendPublicMessage(output);
+				}catch(Exception e){}
+			}
+		}
+    }
+    /**
+     * Sends user who wants to talk to us a feedback that we're (still) stupid.
+     */
+    public void onPrivateMessage(String user, String message){
+        try{
+            SendPrivateMessage(user,"I don't know to talk anything...yet;)");
+        }catch(Exception e){}
+    }
+    
+    
+    public static void main(String[] args) {
+        ExampleBot example_bot=new ExampleBot();
+    }
+}
